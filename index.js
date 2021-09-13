2f0ea85780b8715bc84e297271ee7b598ea60d65
Esteban Vanegas <evanegasal@gmail.com>
2021-09-12 21:06:05 -0500

    v1 Fixed issues
    # Added
    
    - Better module understanding

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const nequiHolidays = require('colombia-holidays');
-const {
-  zonedTimeToUtc,
-  utcToZonedTime,
-  getTimezoneOffset,
-} = require('date-fns-tz');
-const {isSameDay, isWeekend, addDays, format} = require('date-fns');
+const {zonedTimeToUtc, utcToZonedTime} = require('date-fns-tz');
+const {isSameDay, isWeekend, addDays} = require('date-fns');
 
 /**
- * Date to UTC in Colombia timezone
+ * Date to UTC time
  *
  * @param {Date} date           - Date to convert
  * @returns {Date}              - UTC date
+ * @example
+ * const date = new Date('2020-01-01 05:00:00') // => Fri Jan 01 2020 05:00:00 GMT-0500 (Colombian Time);
+ * to_utc(date) // => Fri Jan 01 2020 00:00:00 GMT-0500 (Colombian Time) or Fri Jan 01 2020 05:00:00 UTC;
  */
-const to_utc = (date, timezone = 'America/Bogota') => {
-  const copydate = zonedTimeToUtc(new Date(date), timezone);
-  const offset = getTimezoneOffset(timezone, new Date(date));
-  const returnDate = new Date(copydate.getTime() + offset);
-  return new Date(copydate.getTime() + offset);
+const to_utc = date => {
+  return utcToZonedTime(new Date(date), 'Etc/UTC');
 };
+
 /**
  * Is the date a colombian holiday?
  *
@@ -26,23 +23,13 @@ const to_utc = (date, timezone = 'America/Bogota') => {
  */
 const isHoliday = (date, timezone = 'America/Bogota') => {
   const utc_date = to_utc(date);
-  const utc_day = utc_date.getDate();
-  const date_day = date.getDate();
   const holidays = nequiHolidays.getColombiaHolidaysByYear(
     utc_date.getFullYear(),
   );
   const result = holidays.filter(({holiday}) => {
-    let dateHoliday = utcToZonedTime(new Date(holiday), timezone);
-    const gmt = format(dateHoliday, 'zzz').substr(-2);
-    const oposegmt = -1 * parseInt(gmt);
-    dateHoliday = utcToZonedTime(
-      dateHoliday,
-      `GMT${oposegmt < 0 ? '-' : '+'}${oposegmt}`,
-    );
-    if (holiday === '2020-12-08') {
-      console.log('here');
-    }
-    return isSameDay(utc_date, dateHoliday);
+    let dateHoliday = to_utc(new Date(holiday));
+    let zonedTime = zonedTimeToUtc(new Date(date), timezone);
+    return isSameDay(zonedTime, dateHoliday);
   });
   return isWeekend(utc_date) || result.length > 0;
 };
@@ -57,26 +44,27 @@ const isHoliday = (date, timezone = 'America/Bogota') => {
 const getClosestWorkDay = (date, direction = '+') => {
   let utc_date = to_utc(date);
   const dayToAdd = direction === '+' ? 1 : -1;
-  while (isHoliday(utc_date)) {
+  do {
     utc_date = addDays(utc_date, dayToAdd);
-  }
+  } while (isHoliday(utc_date));
   return utc_date;
 };
 
 /**
- * Add colombian bussiness days to a date
+ * Add or substract colombian bussiness days to a date
  *
  * @param {Date} date               - Date to add bussiness days
- * @param {number} amount           - Amount of days to add
+ * @param {number} amount           - Amount of days to add or substract
  * @returns {Date}                  - Date with bussiness days added
  */
 const addBussinessDays = (date, amount) => {
   let utc_date = to_utc(date);
-  for (let i = amount; i > 0; i--) {
+  const dayToAdd = amount > 0 ? 1 : -1;
+  while (amount + dayToAdd !== 0) {
     utc_date = getClosestWorkDay(utc_date, amount > 0 ? '+' : '-');
-    utc_date = addDays(utc_date, amount > 0 ? 1 : -1);
+    amount -= dayToAdd;
   }
-  return getClosestWorkDay(utc_date, amount > 0 ? '+' : '-');
+  return utc_date;
 };
 
 module.exports = {
--- mocks/dates.js
+++ mocks/dates.js
@@ -1,9 +1,9 @@
 const testDates = {
-  sundayWithHolidayOnMonday: new Date('2020-01-10 06:00:00'),
-  mondayWithHolidayOnTuesday: new Date('2020-07-19 06:00:00'),
-  notHoliday: new Date('2020-09-10 06:00:00'),
-  holidayAmericaBogota: new Date('2020-12-08 06:00:00'),
-  holidayGMT: new Date('2020-12-08'),
+  sundayWithHolidayOnMonday: new Date('2021-01-10 06:00:00'),
+  mondayWithHolidayOnTuesday: new Date('2021-07-19 06:00:00'),
+  notHoliday: new Date('2021-09-10 06:00:00'),
+  holidayAmericaBogota: new Date('2021-12-08 06:00:00'),
+  holidayGMT: new Date('2021-12-08'),
 };
 
 module.exports = testDates;
